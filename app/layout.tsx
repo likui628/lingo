e2ce27c721079593c81ac2056b89d0a68aa7174c
likui628 <90845831+likui628@users.noreply.github.com>
2024-03-19 18:23:42 +0800

    marketing layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 
@@ -16,10 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <ClerkProvider>
-        <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
         <body className={fontNunito.className}>{children}</body>
-        </html>
-      </ClerkProvider>
+      </html>
+    </ClerkProvider>
   );
 }
