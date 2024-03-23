9a2ae138453afd0751e83fd9202a0435ea903067
likui628 <90845831+likui628@users.noreply.github.com>
2024-03-23 22:40:10 +0800

    display user-progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -5,18 +5,32 @@ import {Unit} from "./unit";
 import {Promo} from "@/components/promo";
 import {Quests} from "@/components/quests";
 import {FeedWrapper} from "@/components/fead-wrapper";
+import {getUserProgress} from "@/lib/queries";
+import {redirect} from "next/navigation";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const [
+    userProgress
+  ] = await Promise.all([
+    getUserProgress()
+  ]);
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
-        <UserProgress/>
+        <UserProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+        />
         <Promo/>
         <Quests/>
       </StickyWrapper>
 
       <FeedWrapper>
-        <Header title="Spanish"/>
+        <Header title={userProgress.activeCourse.title}/>
         <Unit/>
       </FeedWrapper>
     </div>
--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,25 +1,39 @@
 import Link from "next/link";
 import {Button} from "@/components/ui/button";
 import Image from "next/image";
+import {Course, UserProgress as UserProgressType} from "@prisma/client";
 
-export const UserProgress = () => {
+type Props = {
+  activeCourse: Course
+  points: UserProgressType['points']
+  hearts: UserProgressType['hearts']
+}
+export const UserProgress = ({activeCourse, points, hearts}: Props) => {
+
+
   return (
     <div className="flex items-center justify-between gap-x-2 w-full">
       <Link href="/courses">
         <Button variant="ghost">
-          <Image src="/es.svg" width={32} height={32} alt="Courses" className="mr-2 rounded-md"/>
+          <Image
+            src={activeCourse.imageSrc}
+            width={32}
+            height={32}
+            alt={activeCourse.title}
+            className="mr-2 rounded-md"
+          />
         </Button>
       </Link>
       <Link href="/shop">
         <Button variant="ghost" size="sm">
           <Image src="/points.svg" width={28} height={28} alt="Points" className="mr-2"/>
-          <span className="text-red-400">100</span>
+          <span className="text-red-400">{points}</span>
         </Button>
       </Link>
       <Link href="/shop">
         <Button variant="ghost" size="sm">
           <Image src="/heart.svg" width={22} height={22} alt="Hearts" className="mr-2"/>
-          <span className="text-red-600">90</span>
+          <span className="text-red-600">{hearts}</span>
         </Button>
       </Link>
     </div>
