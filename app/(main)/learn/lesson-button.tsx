0f25a39cc79a4ade5c7e1a2549630e785285a68d
likui628 <90845831+likui628@users.noreply.github.com>
2024-03-25 21:09:30 +0800

    lesson button style

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -1,37 +1,98 @@
+"use client"
+
 import Link from "next/link";
 import {Check, Crown, Star} from "lucide-react";
 import {Button} from "@/components/ui/button";
 import {cn} from "@/lib/utils";
+import {CircularProgressbarWithChildren} from "react-circular-progressbar";
+
+import 'react-circular-progressbar/dist/styles.css';
 
 type Props = {
   id: string
   index: number
   totalCount: number
   title: string
+  percentage: number
 }
 
-export const LessonButton = ({id, index, totalCount, title}: Props) => {
+export const LessonButton = ({id, index, totalCount, title, percentage}: Props) => {
   const isFirst = index === 0;
   const isLast = index === totalCount - 1;
+  const isCompleted = index == 0;//TODO: replace with actual check
+  
+  const isLocked = index > 1;//TODO: replace with actual check
 
-  const Icon = isFirst ? Check : isLast ? Crown : Star;
-
-  const isCompleted = true;
-  const isLocked = true;
-
+  const isCurrent = index === 1;//TODO: replace with actual check
 
   return (
-    <Link href="/lesson">
+    <Link href="/lesson" style={{
+      marginTop: isFirst && !isCompleted ? '60' : '24',
+    }}>
       <div className="relative mt-6">
-        <Button
-          size="rouded"
-          className="rounded-full border-b-8 h-[70px] w-[70px]"
-        >
-          <Icon
-            className={cn('h-10 w-10', "", "")}
-          />
-        </Button>
+        {
+          isCurrent ? (
+            <div className="w-[102px] h-[102px] relative">
+              <div
+                className="absolute -top-6 left-2.5 px-3 py-2.5 border-2 font-bold uppercase text-green-500 bg-white rounded-xl tracking-wide z-10 animate-bounce"
+              >
+                START
+                <div
+                  className="absolute left-1/2 -bottom-2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform -translate-x-1/2 "
+                />
+              </div>
+              <CircularProgressbarWithChildren
+                value={isNaN(percentage) ? 0 : percentage}
+                styles={{
+                  path: {
+                    stroke: "#4ade80",
+                  },
+                  trail: {
+                    stroke: "#e5e7eb",
+                  },
+                }}
+              >
+                <CustomButton
+                  isCompleted={isCompleted}
+                  isLast={isLast}
+                  isLocked={isLocked}
+                />
+              </CircularProgressbarWithChildren>
+            </div>
+          ) : (
+            <CustomButton
+              isCompleted={isCompleted}
+              isLast={isLast}
+              isLocked={isLocked}
+            />
+          )
+        }
       </div>
     </Link>
+  )
+}
+
+const CustomButton = ({isLocked, isCompleted, isLast}: {
+  isLocked: boolean,
+  isCompleted: boolean,
+  isLast: boolean
+}) => {
+  const Icon = isCompleted ? Check : isLast ? Crown : Star;
+  return (
+    <Button
+      size="rouded"
+      variant={isLocked ? 'locked' : 'secondary'}
+      className="rounded-full border-b-8 h-[70px] w-[70px]"
+    >
+      <Icon
+        className={
+          cn('h-10 w-10',
+            isLocked
+              ? "fill-neutral-400 text-neutral-400 stroke-neutral-400"
+              : "fill-primary-foreground text-primary-foreground",
+            isCompleted && "fill-none stroke-[4]"
+          )}
+      />
+    </Button>
   )
 }
